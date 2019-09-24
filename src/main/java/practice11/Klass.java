fb692fddd042b395dfb2c339ae13a27783772e88
Iyan Alvarez <iyan.oliver.alvarez@oocl.com>
2019-09-24 10:14:05 +0800

    Pass Practice11

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,35 @@
 package practice11;
 
 public class Klass {
+
+    int number;
+    String displayName;
+    Object leader;
+    Object member;
+
+    public Klass(int i) {
+        this.number = i;
+        this.displayName = "Class " +i;
+    }
+
+    public int getNumber()
+    {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void assignLeader(Student jerry) {
+        this.leader = jerry;
+    }
+
+    public Object getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student jerry) {
+        this.member = jerry;
+    }
 }
